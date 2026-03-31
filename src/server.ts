436d17f62ea1fc76f0b88941d2bd71a19d1f18e7
ADS 2024.1N <36129382024.1N@senacrjedu.local>
2026-03-31 20:39:44 -0300

    feat: atualizações do projeto Fastify/Prisma

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+//import fastify, {FastifyInstance} from "fastify"
+import fastify from "fastify"
+import type { FastifyInstance } from "fastify"
+
+// logger trás algumas informações de log do sistema
+const app: FastifyInstance = fastify({ logger: true});
+
+app.listen(
+    {
+        port: 3100,
+    },
+    () => console.log('Server Runing...'),
+);
